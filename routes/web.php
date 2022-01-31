0848a7edc6cb8374dea5fdb2a07d7f6bb6470ba8
HP Laptop <amritbhagowalia@gmail.com>
2022-01-31 14:56:48 +0530

    new

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('admin/login', [AdminAuthController::class, 'index'])->name('admin.login');
+Route::get('/', [AdminAuthController::class, 'index'])->name('admin.login');
 Route::post('admin/login', [AdminAuthController::class, 'store'])->name('admin.login.store');
 
 
